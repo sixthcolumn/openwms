6bbf21f9db4823655794eb7282eb9b3414a038d0
Ben Goodwin <benj.goodwin@gmail.com>
2015-05-10 10:52:37 -0400

    Added new inserts

--- scripts/sql/db_scripts3.sql
+++ scripts/sql/db_scripts3.sql
@@ -1,12 +1,13 @@
 insert into package (id, name) values (7, 'WMS Server');
 insert into package (id, name) values (8, 'Open ADR');
-
+insert into package (id, name) values (13, 'WO_Server');
 insert into vendor_role_type (id, name) values (6, "Work Mgmt");
 insert into interface (id, name, package_id, proxy_flag, required_flag, vendor_role_type_id) values (33, 'WMS(RequestWorkRequest)', 7, 0, 0, 6);
 insert into interface (id, name, package_id, proxy_flag, required_flag, vendor_role_type_id) values (34, 'WMS(WorkRequest)', 7, 0, 0, 6);
 
 insert into vendor_role_type (id, name) values (5, "OpenADR");
 insert into interface (id, name, package_id, proxy_flag, required_flag, vendor_role_type_id) values (60, 'OpenADR(RequestEventState)', 8, 0, 0, 5);
+insert into interface (id, name, package_id, proxy_flag, required_flag, vendor_role_type_id) values (61, 'ADR(EventStatePost)', 8, 0, 0, 5);
 insert into interface (id, name, package_id, proxy_flag, required_flag, vendor_role_type_id) values (61, 'ADR(EventStatePost)', 8, 0, 0, 5);
 
 delete from packages_vendor_role_types where package_id in (6,7,8);
