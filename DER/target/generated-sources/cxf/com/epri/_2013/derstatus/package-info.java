66cba3446ca21a3d3ea4b4fa7947098f349cb3df
Ben Goodwin <benj.goodwin@gmail.com>
2015-11-17 14:23:21 -0500

    Accidentally commited target, this kills it

--- DER/target/generated-sources/cxf/com/epri/_2013/derstatus/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.epri.com/2013/DERStatus#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.epri._2013.derstatus;
--- DER/target/generated-sources/cxf/org/multispeak/v5/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.multispeak.org/V5.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.multispeak.v5;
